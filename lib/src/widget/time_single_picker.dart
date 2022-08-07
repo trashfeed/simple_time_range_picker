b4cea276f124121bcf5dd637c5d62e1e542c1fe4
trashfeed <info@trashfeed.net>
2022-08-07 12:20:45 +0900

    fix lint

--- lib/src/widget/time_single_picker.dart
+++ lib/src/widget/time_single_picker.dart
@@ -10,7 +10,6 @@ import 'dart:math' as math;
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 
 // Examples can assume:
 // BuildContext context;
